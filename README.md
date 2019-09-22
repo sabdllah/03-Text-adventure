ec6f6cdab29811dd77daff064a2748d9638a2667
Safiyyah Abd'Llah <sabdllah@iu.edu>
2019-09-21 21:25:12 -0400

    text adventure complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 03-Text-adventure
 Our Text Adventure Game
+The game.py file is an example of a simple text adventure game. The purpose of this game is to practice creating a text adventure game in python. The game is composed of if statements that generate a response based on the player's input. The game is not meant to be fun or interesting, rather an experiment in creating a functional text adventure game in python. 
+
+The zork.json file does not correspond with the game.py file. It corresponds with main.py.
--- main.py
+++ main.py
@@ -1,48 +1,55 @@
-import sys, logging, json
+import sys, os, json
 
-#check to make sure we are running the right version of Python
 version = (3,7)
 assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
 
-#turn on logging, in case we have to leave ourselves debugging messages
-logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
-def renter ():
-    '''Dispaly the current location'''
-    return True 
 
-def update ():
-    '''update our location, if possible, etc. '''
-    return True
 
-def check_input ():
-    '''get user input'''
-    response = input('What would you like to do?')
-    return response 
+# Game loop functions
+def render(game,current):
+    ''' Displays the current room '''
+
+    print('You are in the ' + game['rooms'][current]['name'])
+    print(game['rooms'][current]['desc'])
+
+def getInput():
+    ''' Asks the user for input and returns a stripped, uppercase version of what they typed '''
+
+    response = input('What would you like to do? ').strip().upper()
+    return response
+
+
+def update(response,game,current):
+    ''' Process the input and update the state of the world '''
+    for e in game['rooms'][current]['exits']:
+        if response == e['verb']:
+            current = e['target']
+    return current
+
 
 
 def main():
+
     game = {}
     with open('zork.json') as json_file:
         game = json.load(json_file)
-    # Your game goes here!
 
     current = 'WHOUS'
-    quit = false
-    while not quit: 
-    #render
-    render()
-    #check player input
-    check_input()
-    #update
-    update()
-    
 
-    return True
+    quit = False
+
+    while not quit:
+
+        render(game,current)
+        response = getInput()
+        current = update(response,game,current)
+
+        if response == 'QUIT':
+            quit = True
 
 
 
-#if we are running this from the command line, run main
+
 if __name__ == '__main__':
 	main()
